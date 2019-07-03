0e13eb814272b375fbe1c508d96f5ea54466a367
Penha <luiz.henrique.penha@accenture.com>
2019-07-03 17:19:44 -0300

    jogo memorigame do joão adicionado

---
+++ perguntas.js
@@ -0,0 +1,27 @@
+var listaPerguntas = new Array();
+
+questao1 = {
+	imagem:"imagens/formas-geometricas.jpg",
+	pergunta:"Qual a cor do círculo?",
+	resposta:"vermelho",
+	alternativas:["vermelho","laranja","amarelo","azul"]
+	};
+
+//pergunta 1
+listaPerguntas.push(questao1);
+
+//para adicionar novas perguntas ao programa:
+listaPerguntas.push({
+imagem:"imagens/times.jpg",
+pergunta:"Qual dos times não é brasileiro?",
+resposta:"PSG",
+alternativas:["Santa Cruz","PSG","Santos Futebol Clube"]
+});
+
+
+listaPerguntas.push({
+	imagem:"https://image.freepik.com/vetores-gratis/icones-da-familia-feliz_23-2147506164.jpg",
+	pergunta:"Qual a cor da mãe?",
+	resposta:"laranja",
+	alternativas:["vermelho","laranja","amarelo","azul"]
+	});
